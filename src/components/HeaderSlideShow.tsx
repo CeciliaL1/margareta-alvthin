7e4b4b3606c52acee7941d89353ad0e03181c3ad
CeciliaL1 <cecilia.lepik@hotmail.com>
2025-11-20 20:05:37 +0100

    :sparkles: add arrow down in header

--- src/components/HeaderSlideShow.tsx
+++ src/components/HeaderSlideShow.tsx
@@ -51,7 +51,25 @@ export const HeaderSlideShow = () => {
           </motion.div>
         </AnimatePresence>
       </header>
-      <h1 className="header-heading">Margareta Alvthin</h1>
+      <div className="header-center">
+        <h1 className="header-heading">Margareta Alvthin</h1>
+
+        <motion.svg
+          initial={{ y: 0 }}
+          whileHover={{ y: 6 }}
+          transition={{
+            type: "spring",
+            stiffness: 200,
+            damping: 15,
+          }}
+          className="header-arrow"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 640 640"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <path d="M297.4 438.6C309.9 451.1 330.2 451.1 342.7 438.6L502.7 278.6C515.2 266.1 515.2 245.8 502.7 233.3C490.2 220.8 469.9 220.8 457.4 233.3L320 370.7L182.6 233.4C170.1 220.9 149.8 220.9 137.3 233.4C124.8 245.9 124.8 266.2 137.3 278.7L297.3 438.7z" />
+        </motion.svg>
+      </div>
     </>
   );
 };
